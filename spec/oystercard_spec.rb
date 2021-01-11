f50fbb236bf3f032fbdc77916e3ea0825aa04dd4
HolsDuckett <hf.duckett@gmail.com>
2021-01-11 16:26:43 +0000

    challenge 4 - add balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
 
-describe Oystercard do 
-end 
+describe Oystercard do
+
+  # it "should check a card has a balance" do
+  #   expect(subject.balance?).to eq(0)
+  # end
+
+  it { is_expected.to respond_to(:balance) }
+
+end
