059c08cfc803c21c64767d443d66baa664c20b90
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-15 15:58:36 +0000

    step14 fare method has penalty and min fares

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,21 @@
 class Journey
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
 
   attr_reader :entry_station, :exit_station
 
   def initialize(entry_station, exit_station)
     @entry_station = entry_station
     @exit_station = exit_station
-  end 
+  end
+
+  def fare
+    !entry_station || !exit_station ? PENALTY_FARE : MINIMUM_FARE
+    # if !entry_station || !exit_station
+    #   PENALTY_FARE
+    # else 
+    #   MINIMUM_FARE
+    # end
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,6 +3,7 @@ require "journey"
 describe Journey do 
 
   subject(:journey) { Journey.new("station1", "station2") }
+  let(:journey1) { Journey.new("station1", nil) }
 
   it 'returns entry_station given' do 
     expect(journey.entry_station).to eq "station1" 
@@ -17,5 +18,18 @@ describe Journey do
       expect(Journey).to respond_to(:new).with(2).arguments
     end 
   end 
+
+  describe "#fare" do
+    it { is_expected.to respond_to :fare }
+
+    it "returns minimum fare" do
+      expect(journey.fare).to eq Journey::MINIMUM_FARE
+    end
+
+    it "returns penalty fare if no entry/exit" do
+      expect(journey1.fare).to eq Journey::PENALTY_FARE
+    end
+
+  end
 
 end
