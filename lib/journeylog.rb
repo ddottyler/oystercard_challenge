8530d1a2f94f128a547432809f98c148819b225a
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-16 12:58:37 +0000

    started tests for journeylog

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,11 +1,23 @@
 class JourneyLog
 
+  attr_reader :journey_list
+
+  def initialize
+    @journey_list = []
+  end
+
+
   def start_journey(entry_station)
     @entry_station = entry_station 
   end 
   
   def in_journey?
     @entry_station == nil ? false : true
+  end
+
+  def end_journey(exit_station)
+    @journey_list << "journey object will go here"
+    @entry_station = nil
   end
 
 end 
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -8,4 +8,33 @@ describe JourneyLog do
     expect(subject.in_journey?).to eq(true)
   end
 
+  it "ends journey" do
+    entry_station = "station1"
+    exit_station = "station2"
+    subject.start_journey(entry_station)
+    subject.end_journey(exit_station)
+    expect(subject.in_journey?).to eq false
+  end
+
+  it "adds to journeys list" do
+    entry_station = "station1"
+    exit_station = "station2"
+    subject.start_journey(entry_station)
+    expect { subject.end_journey(exit_station) }.to change { subject.journey_list.length }.by 1
+  end
+
+
+
+  it "adds a Journey to journeys list" do
+    journey = double Journey
+    allow(subject).to receive(:journey_list).and_return([journey])
+    allow(journey).to receive(:entry_station)
+    entry_station = "station1"
+    exit_station = "station2"
+    subject.start_journey(entry_station)
+    subject.end_journey(exit_station)
+    expect(subject.journey_list[-1]).to respond_to :entry_station
+  end
+
+
 end 
